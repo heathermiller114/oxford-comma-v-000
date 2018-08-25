fd0b4c46e825630a0bd9871852ecef6eacd136cf
Heather Miller <heather.miller114@gmail.com>
2018-08-25 23:02:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >=3 
-    array[0..(array.length - 1)].join(", ")
+    array[0..(array.length - 2)].join(", ")
   end
 end
